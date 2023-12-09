05f55196bd6ce57785fafc2269d02d556c1b63bc
generalentropy <entropie@tuta.io>
2023-12-09 13:26:09 +0100

    update

--- src/components/Close.jsx
+++ src/components/Close.jsx
@@ -7,19 +7,7 @@ IconClose.propTypes = {
 export default function IconClose({ setIsMenuOpen }) {
   return (
     <div className="btn-close-menu" onClick={() => setIsMenuOpen(false)}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        // width="64"
-        // height="64"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fill="#fff"
-          fillRule="evenodd"
-          d="M4.28 3.22a.75.75 0 00-1.06 1.06L6.94 8l-3.72 3.72a.75.75 0 101.06 1.06L8 9.06l3.72 3.72a.75.75 0 101.06-1.06L9.06 8l3.72-3.72a.75.75 0 00-1.06-1.06L8 6.94 4.28 3.22z"
-          clipRule="evenodd"
-        ></path>
-      </svg>
+      <img src="/close.svg" />
     </div>
   );
 }
