d4f042a61b48fdae2ab3919b69a569c301b9ae22
generalentropy <entropie@tuta.io>
2023-12-09 14:07:57 +0100

    update

--- src/components/Close.jsx
+++ src/components/Close.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import CloseIcon from "/close.svg";
 
 IconClose.propTypes = {
   setIsMenuOpen: PropTypes.func,
@@ -8,7 +7,13 @@ IconClose.propTypes = {
 export default function IconClose({ setIsMenuOpen }) {
   return (
     <div className="btn-close-menu" onClick={() => setIsMenuOpen(false)}>
-      <img src={CloseIcon} alt="icon" />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+        <path
+          fill="#fff"
+          stroke="#fff"
+          d="M4.28 3.22a.75.75 0 0 0-1.06 1.06L6.94 8l-3.72 3.72a.75.75 0 1 0 1.06 1.06L8 9.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L9.06 8l3.72-3.72a.75.75 0 0 0-1.06-1.06L8 6.94z"
+        />
+      </svg>
     </div>
   );
 }
